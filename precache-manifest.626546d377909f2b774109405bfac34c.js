ee6095d251b96ab29629dc80e755109f08d7e956
MohamadHawshar <mohamad_hawchar@hotmail.com>
2020-10-18 16:39:16 +0300

    Updates

---
+++ precache-manifest.626546d377909f2b774109405bfac34c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0674066b5d48a629d1503111a97a76",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "6510a0316397ae178006",
+    "url": "/TodoList/static/css/main.5609200d.chunk.css"
+  },
+  {
+    "revision": "e6baddf7d26c208e4a0e",
+    "url": "/TodoList/static/js/2.056e67f4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/TodoList/static/js/2.056e67f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6510a0316397ae178006",
+    "url": "/TodoList/static/js/main.066c3dd8.chunk.js"
+  },
+  {
+    "revision": "dd337711bee24ff7e940",
+    "url": "/TodoList/static/js/runtime-main.476b3ef9.js"
+  }
+]);
